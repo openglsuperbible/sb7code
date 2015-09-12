803d67877d061c378bdb26dc02990d7447f3b4ac
Dimitris Pliatsios <pliago@hotmail.com>
2015-09-12 23:41:14 +0300

    Use strcpy_s to avoid errors in visual studio 2013

--- include/sb7.h
+++ include/sb7.h
@@ -177,7 +177,7 @@ public:
 
     virtual void init()
     {
-        strcpy(info.title, "OpenGL SuperBible Example");
+        strcpy_s(info.title, "OpenGL SuperBible Example");
         info.windowWidth = 800;
         info.windowHeight = 600;
 #ifdef __APPLE__
